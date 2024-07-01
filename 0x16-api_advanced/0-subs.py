fbdef8df65a5e36153a8570ff5ea1e3df8389b3c
Maryam-Ketatni-004 <maryamketatni00.com>
2024-07-01 02:57:16 -0700

    oo

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
-"""Module to query number of subscribers of a subreddit"""
+"""return subscribers"""
 import requests
 
 
 def number_of_subscribers(subreddit):
-    """Function to query number of subscribers of a subreddit"""
-    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {'User-Agent': 'Mozilla/5.0'}
-    response = requests.get(url, headers=headers, allow_redirects=False)
-    if response.status_code != 200:
+    """return subscribers"""
+    if not subreddit or not isinstance(subreddit, str):
         return 0
-    return response.json().get('data').get('subscribers')
+    r = requests.get(
+        'https://www.reddit.com/r/{}/about.json'.format(subreddit),
+        headers={'User-Agent': "omar"})
+    if r.status_code == 200:
+        data = r.json()["data"]
+        return data["subscribers"]
+    return 0
